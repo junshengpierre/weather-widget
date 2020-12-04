b8c6917af24069044900fff0c589f6c11fbc1a04
Teo Jun Sheng, Pierre <junshengpierre@gmail.com>
2020-12-05 03:47:28 +0800

    scaffold app + remove unused code

--- src/App.js
+++ src/App.js
@@ -1,60 +1,75 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
-import { GlobalStyles } from "twin.macro";
+import logo from "./logo.png";
+import tw, { GlobalStyles, styled } from "twin.macro";
+import { keyframes, createGlobalStyle } from "styled-components";
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <GlobalStyles />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <GlobalStyle />
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
+      </AppHeader>
+      <Form>
+        <TextInput data-testid="search-input" type="text" />
+        <SubmitButton type="submit" value="Search" />
+      </Form>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+`;
+
+const AppContainer = styled.div`
+  text-align: center;
+  ${tw`px-4`}
+`;
+
+const float = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(10px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${float} infinite 3s ease-in-out;
+  pointer-events: none;
+  ${tw``}
+`;
+
+const AppHeader = styled.header`
+  ${tw`flex justify-center pt-4 pb-12`}
+`;
+
+const Form = styled.form`
+  ${tw`flex w-full`}
+`;
+
+const TextInput = styled.input`
+  ${tw`border rounded border-gray-400 px-2.5 py-1 w-full`}
+`;
+
+const SubmitButton = styled.input`
+  ${tw`border rounded border-gray-400 px-4 py-1 ml-4`}
+`;
--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(
+test("renders app logo", () => {
+  const { getByAltText, getByText, getByTestId } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByAltText(/logo/i)).toBeInTheDocument();
+  expect(getByText(/search/i)).toBeInTheDocument();
+  expect(getByTestId("search-input")).toBeInTheDocument();
 });
